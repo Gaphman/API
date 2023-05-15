23841d7885d29ff09aa4b6e14c109cd863a6ff9a
Gaphman <ghaffarbolakale@gmail.com>
2023-05-15 19:00:48 +0100

    solution

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserList from './components/UserList';
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
